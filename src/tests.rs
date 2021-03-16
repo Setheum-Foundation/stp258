4be9dbf036aa37620c7233f8689d7c623d10c6e4
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-17 00:08:36 +0800

    tests

--- src/tests.rs
+++ src/tests.rs
@@ -14,27 +14,16 @@ fn expand_supply_should_work() {
 		.five_hundred_thousand_for_sett_pay_n_serper()
 		.build()
 		.execute_with(|| {
-			assert_ok!(SerpMarket::reserve(STP258_NATIVE_ID, &SERPER_ACC, 150_000));
-			assert_eq!(SerpMarket::free_balance(STP258_NATIVE_ID, &SERPER_ACC), 350_000);
-			assert_eq!(SerpMarket::free_balance(STP258_TOKEN_ID, &SERPER_ACC), 500_000 * 1_000);
-			assert_eq!(SerpMarket::total_issuance(STP258_NATIVE_ID), 1_000_000);
 			assert_eq!(Stp258Tokens::total_issuance(STP258_TOKEN_ID), 1_000_000 * 1_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_NATIVE_ID, &SERPER_ACC), 150_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_TOKEN_ID, &SERPER_ACC), 0 * 1_000);
-			assert_ok!(SerpMarket::reserve(STP258_TOKEN_ID, &SERPER_ACC, 150_000 * 1_000));
-			assert_eq!(SerpMarket::reserved_balance(STP258_TOKEN_ID, &SERPER_ACC), 150_000 * 1_000);
-			assert_eq!(SerpMarket::free_balance(STP258_TOKEN_ID, &SERPER_ACC), 350_000 * 1_000);
-			assert_eq!(SerpMarket::free_balance(STP258_NATIVE_ID, &SERPER_ACC), 350_000);
-			assert_eq!(SerpMarket::total_issuance(STP258_NATIVE_ID), 1_000_000);
-			assert_eq!(Stp258Tokens::total_issuance(STP258_TOKEN_ID), 1_000_000 * 1_000);
-
-			assert_ok!(SerpMarket::expand_supply(Origin::root(), STP258_TOKEN_ID, 110_000 * 1000, 89 * 1000)); 
-			assert_eq!(SerpMarket::free_balance(STP258_NATIVE_ID, &SERPER_ACC), 350_000);
-			assert_eq!(SerpMarket::free_balance(STP258_TOKEN_ID, &SERPER_ACC), 460_000 * 1_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_NATIVE_ID, &SERPER_ACC), 148_900);
-			assert_eq!(SerpMarket::reserved_balance(STP258_TOKEN_ID, &SERPER_ACC), 150_000 * 1_000);
-			assert_eq!(SerpMarket::total_issuance(STP258_NATIVE_ID), 998_900);
-			assert_eq!(Stp258Tokens::total_issuance(STP258_TOKEN_ID), 1_100_000 * 1_000);
+			let prev_supply = Stp258Tokens::total_issuance(STP258_TOKEN_ID);
+			let expand_by = 110_000;
+			let price = 89;
+			assert_ok!(SerpMarket::expand_supply(Origin::root(), STP258_TOKEN_ID, expand_by, price)); 
+			assert_eq!(
+				Stp258Tokens::total_issuance(STP258_TOKEN_ID), 
+				prev_supply.saturating_add(expand_by),
+			"supply should be increased by expand_by"
+			);
 		});
 }
 
@@ -44,26 +33,15 @@ fn contract_supply_should_work() {
 		.five_hundred_thousand_for_sett_pay_n_serper()
 		.build()
 		.execute_with(|| {
-			assert_ok!(SerpMarket::reserve(STP258_NATIVE_ID, &SERPER_ACC, 150_000));
-			assert_eq!(SerpMarket::free_balance(STP258_NATIVE_ID, &SERPER_ACC), 350_000);
-			assert_eq!(SerpMarket::free_balance(STP258_TOKEN_ID, &SERPER_ACC), 500_000 * 1_000);
-			assert_eq!(SerpMarket::total_issuance(STP258_NATIVE_ID), 1_000_000);
 			assert_eq!(Stp258Tokens::total_issuance(STP258_TOKEN_ID), 1_000_000 * 1_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_NATIVE_ID, &SERPER_ACC), 150_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_TOKEN_ID, &SERPER_ACC), 0 * 1_000);
-			assert_ok!(Stp258Tokens::reserve(STP258_TOKEN_ID, &SERPER_ACC, 150_000 * 1_000));
-			assert_eq!(SerpMarket::reserved_balance(STP258_TOKEN_ID, &SERPER_ACC), 150_000 * 1_000);
-			assert_eq!(SerpMarket::free_balance(STP258_TOKEN_ID, &SERPER_ACC), 350_000 * 1_000);
-			assert_eq!(SerpMarket::free_balance(STP258_NATIVE_ID, &SERPER_ACC), 350_000);
-			assert_eq!(SerpMarket::total_issuance(STP258_NATIVE_ID), 1_000_000);
-			assert_eq!(Stp258Tokens::total_issuance(STP258_TOKEN_ID), 1_000_000 * 1_000);
-
-			assert_ok!(SerpMarket::contract_supply(Origin::root(), STP258_TOKEN_ID, 100_000 * 1000, 20 * 1000)); 
-			assert_eq!(SerpMarket::free_balance(STP258_NATIVE_ID, &SERPER_ACC), 355_500);
-			assert_eq!(SerpMarket::free_balance(STP258_TOKEN_ID, &SERPER_ACC), 350_000 * 1_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_NATIVE_ID, &SERPER_ACC), 150_000);
-			assert_eq!(SerpMarket::reserved_balance(STP258_TOKEN_ID, &SERPER_ACC), 50_000 * 1_000);
-			assert_eq!(SerpMarket::total_issuance(STP258_NATIVE_ID), 1_005_500);
-			assert_eq!(Stp258Tokens::total_issuance(STP258_TOKEN_ID), 900_000 * 1_000);
+			let prev_supply = Stp258Tokens::total_issuance(STP258_TOKEN_ID);
+			let contract_by = 100_000;
+			let price = 20;
+			assert_ok!(SerpMarket::contract_supply(Origin::root(), STP258_TOKEN_ID, contract_by, price)); 
+			assert_eq!(
+				Stp258Tokens::total_issuance(STP258_TOKEN_ID), 
+				prev_supply.saturating_sub(contract_by),
+			"supply should be decreased by contract_by"
+			);
 		});
 }
